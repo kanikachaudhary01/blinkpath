e5b58febe008ed35cf922387def37009628bd8f7
kanikachaudhary01 <kanikachoudharykk115@gmail.com>
2025-07-06 23:03:23 +0530

    infinity scroll

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,20 @@
+import { motion } from "motion/react";
 import AboutCard, { aboutData } from "./AboutCard";
-
-const About = () => {
+type MarueData={
+    title:string;
   
+}
+const About = () => {
+ const marQuee:MarueData [] = [ {
+    title:"WEB PRESENCE",},
+{title:"UX RESEARCH"},
+{title:"UX/UI DESIGN"},
+{title:"DIGITAL PRODUCT STRATERGY"},
+{title:"VISUAL DESIGN"},
+{title:"DESIGN SYSTEMS"},
+{title:"AI UX"},
+
+]
     
     return (
         <>
@@ -32,11 +45,32 @@ const About = () => {
                     Startups. Scale-ups. New bets. Big shifts. We work with
                     teams navigating what’s next—launching, evolving,
                     rebuilding. No two challenges are the same, but most share
-                    the same ambition. Move with purpose and don’t waste time
+                    the same ambition. Move with purpose and don’t waste time   
                     getting there. This is how we show up, every time. Where do
                     we start?
                 </div>
-                <div className="mt-20">
+                 <div className="bg-black  h-25 rounded-b-4xl flex gap-5" >
+                          <motion.div
+        className="flex gap-10 whitespace-nowrap"
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{
+          duration: 20,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      >
+        {[...marQuee, ...marQuee].map((item, index) => (
+          <div
+            key={index}
+            className="text-white text-5xl font-bold px-4 mt-6"
+          >
+            {item.title}
+          </div>
+        ))}
+      </motion.div>
+                 </div>
+                <div className="">                   
                     <footer className="bg-[#FF3C00] text-white px-8 md:px-20 py-16">
                        
                         <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
--- src/components/Index.js
+++
@@ -1,5 +0,0 @@
-const navbarItems = ["Work","Services","Approach","About","Contact"]
- 
-navbarItems.map((items,index)=>{
-console.log(items)
-})
